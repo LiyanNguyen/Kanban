e86e64742e331765af5669838ce5e602bfe5b3a6
Liyan Anajao <123532430+LiyanAnajao1Bit@users.noreply.github.com>
2023-05-29 12:00:23 +0800

    old board data logic update

--- src/layouts/Board.tsx
+++ src/layouts/Board.tsx
@@ -3,26 +3,24 @@ import EmptyBoard from '../components/EmptyBoard';
 import React, { useEffect, useState } from 'react';
 import Column from '../components/Column';
 import { boardStore } from '../zustand/boardStore';
-import boardData from '../data/boardData' //fake data for now - same on all boards (boardIndex)
 import { task } from '../types/task';
 
 const Board = () => {
-  const [boardIndex] = boardStore((state) => [state.boardIndex])
+  const [boardData] = boardStore((state) => [state.boardData])
   const [todoTasks, setTodoTasks] = useState<task []>([])
   const [doingTasks, setDoingTasks] = useState<task []>([])
   const [doneTasks, setDoneTasks] = useState<task[]>([])
   const [isEmpty, setIsEmpty] = useState<boolean>(false)
 
   useEffect(() => {
-    console.log(boardIndex)
     setTodoTasks(boardData.filter(item => item.status === 'Todo'))
     setDoingTasks(boardData.filter(item => item.status === 'Doing'))
     setDoneTasks(boardData.filter(item => item.status === 'Done'))
 
-    if (todoTasks.length === 0 && doingTasks.length === 0 && doneTasks.length === 0) setIsEmpty(true)
+    if (boardData.length === 0) setIsEmpty(true)
     else setIsEmpty(false)
 
-  }, [boardIndex, todoTasks.length, doingTasks.length, doneTasks.length])
+  }, [boardData])
 
   return (
     <Stack direction='row' bgcolor='#F4F7FD' width='100%' height='100%' justifyContent={isEmpty ? 'center' : 'space-between'} p={3} gap={3}>
--- src/zustand/boardStore.ts
+++ src/zustand/boardStore.ts
@@ -1,6 +1,9 @@
 import { create } from 'zustand'
+import { task } from '../types/task'
+import boardData from '../data/boardData'
 
 interface IboardStore {
+  boardData: task []
   boards: string[]
   boardIndex: number
   setBoards: (newBoards: string[]) => void
@@ -8,6 +11,7 @@ interface IboardStore {
 }
 
 export const boardStore = create<IboardStore>((set) => ({
+  boardData: boardData, //fake data first, same data for all boards for now
   boards: ['Platform Launch', 'Marketing Plan', 'Roadmap',],
   boardIndex: 0,
   setBoards: (newBoards) => set(() => ({ boards: newBoards })),
