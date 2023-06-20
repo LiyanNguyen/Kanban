6face3b7c795db8ccd2ccc1317c210d73ebc5e8c
LiyanNguyen <liyannguyenn@gmail.com>
2023-06-20 22:11:50 +0800

    Moved add / edit board into sidebar

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-import { Stack, Box, ListItemButton, ListItemText, List, Button, Typography } from '@mui/material';
+import { useState, MouseEvent } from 'react'
+import { Stack, Box, ListItemButton, ListItemText, List, Button, Typography, IconButton, Menu, MenuItem, Tooltip } from '@mui/material';
 import logo from '../assets/logo-dark.svg'
 import BoardModal from '../components/BoardModal';
 import PerfectScrollbar from 'react-perfect-scrollbar'
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import { boardStore } from '../zustand/boardStore';
 import LibraryAddOutlinedIcon from '@mui/icons-material/LibraryAddOutlined';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import DeleteBoardModal from '../components/DeleteBoardModal';
 
 const Sidebar = () => {
   const [boards, boardIndex, setBoardIndex] = boardStore((state) => [state.boards, state.boardIndex, state.setBoardIndex])
   const [openModal, setOpenModal] = useState<boolean>(false)
+  const [openDeleteModal, setOpenDeleteModal] = useState<boolean>(false)
+  const [openEditModal, setOpenEditModal] = useState<boolean>(false)
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  }
 
   return (
     <>
@@ -17,25 +28,36 @@ const Sidebar = () => {
         <Box px={4} pt={4} pb={3}>
           <img src={logo} alt="logo" width={152} height={25} />
         </Box>
-        <Stack px={4}>
-          <Button onClick={() => setOpenModal(true)} variant='outlined' size='small' sx={{ textTransform: 'none', color: '#635FC7', borderColor: '#635FC7' }}>
+        <Stack pl={4}>
+          <Button onClick={() => setOpenModal(true)} variant='outlined' size='small' sx={{ textTransform: 'none', color: '#635FC7', borderColor: '#635FC7', mr: 4 }}>
             <LibraryAddOutlinedIcon fontSize="small" /> Create New Board
           </Button>
           <Typography color='#828FA3'   fontSize='medium' mt={2}>
            My Boards ({boards.length})
           </Typography>
           <PerfectScrollbar style={{ maxHeight: 500 }}>
-            <List>
+            <List sx={{mr:1}}>
               {boards.map((item, index) =>
-                <ListItemButton sx={{borderRadius: 1.5}} key={index} selected={index === boardIndex} onClick={() => setBoardIndex(index)}>
-                  <ListItemText primary={item} />
-                </ListItemButton>
+                <Stack direction='row' alignItems='center' key={index}>
+                  <ListItemButton sx={{borderRadius: 1.5}} key={index} selected={index === boardIndex} onClick={() => setBoardIndex(index)}>
+                    <ListItemText primary={item} />
+                  </ListItemButton>
+                  <IconButton sx={{width: 36, height:36}} onClick={handleClick}>
+                    <MoreVertOutlinedIcon  htmlColor='#828FA3' sx={{fontSize: 18}} />
+                  </IconButton>
+                </Stack>
               )}
+              <Menu anchorEl={anchorEl} open={open} onClose={() => setAnchorEl(null)}>
+                <MenuItem onClick={() => { setOpenEditModal(true); setAnchorEl(null) }} sx={{ color: '#828FA3' }}>Edit</MenuItem>
+                <MenuItem onClick={() => { setOpenDeleteModal(true); setAnchorEl(null) }} sx={{ color: '#828FA3' }}>Delete</MenuItem>
+              </Menu>
             </List>
           </PerfectScrollbar>
         </Stack>
       </Stack>
-      <BoardModal openModal={openModal} setOpenModal={setOpenModal}/>
+      <BoardModal openModal={openModal} setOpenModal={setOpenModal} />
+      <BoardModal isEditing boardName='Platform Launch' openModal={openEditModal} setOpenModal={setOpenEditModal} />
+      <DeleteBoardModal itemName='Platform Launch' openModal={openDeleteModal} setOpenModal={setOpenDeleteModal} />
     </>
   )
 }
--- src/layouts/Topbar.tsx
+++ src/layouts/Topbar.tsx
@@ -1,12 +1,8 @@
-import { Button, IconButton, Menu, MenuItem, Stack, Tooltip, Typography } from '@mui/material';
+import { Button, IconButton, Stack, Tooltip, Typography } from '@mui/material';
 import AddTaskIcon from '@mui/icons-material/AddTask';
 import { modalStore } from '../zustand/modalStore';
 import MenuOpenIcon from '@mui/icons-material/MenuOpenRounded';
-import { Dispatch, SetStateAction, useState, MouseEvent} from 'react';
-import DeleteBoardModal from '../components/DeleteBoardModal';
-import BoardModal from '../components/BoardModal';
-import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
-
+import { Dispatch, SetStateAction } from 'react';
 
 type Props = {
   setOpen: Dispatch<SetStateAction<boolean>>
@@ -15,46 +11,24 @@ type Props = {
 const Topbar = (props: Props) => {
   const { setOpen } = props
   const [setOpenTaskModal] = modalStore((state) => [state.setOpenTaskModal])
-  const [openDeleteModal, setOpenDeleteModal] = useState<boolean>(false)
-  const [openEditModal, setOpenEditModal] = useState<boolean>(false)
-
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl)
-
-  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  }
 
   return (
-    <>
-      <Stack direction='row' alignItems='center' px={[1,3,3]} py={1.5} bgcolor='white' borderBottom={'1px solid #E4EBFA'} justifyContent='space-between'>
-        <Stack direction='row' alignItems='center'>
-          <Tooltip title="Open Boards">
-            <IconButton size='small' onClick={() => setOpen(true)}>
-              <MenuOpenIcon htmlColor='#635FC7' fontSize='large' sx={{rotate: '180deg'}}/>
-            </IconButton>
-          </Tooltip>
-        </Stack>
-        <Typography fontSize={[16, 24, 24]} fontWeight='bold'>Platform Launch</Typography>          
-        <Stack direction='row' alignItems='center'>
-          <IconButton sx={{display:['block','none']}} size='small' onClick={() => setOpenTaskModal(true)}>
-            <AddTaskIcon htmlColor='#635FC7' fontSize='medium' />
+    <Stack direction='row' alignItems='center' px={[1,3,3]} py={1.5} bgcolor='white' borderBottom={'1px solid #E4EBFA'} justifyContent='space-between'>
+      <Stack direction='row' alignItems='center'>
+        <Tooltip title="Open Boards">
+          <IconButton size='small' onClick={() => setOpen(true)}>
+            <MenuOpenIcon htmlColor='#635FC7' fontSize='large' sx={{rotate: '180deg'}}/>
           </IconButton>
-          <Button size='small' onClick={() => setOpenTaskModal(true)} variant='outlined' sx={{ textTransform: 'none', gap: 1, display: ['none', 'flex'], color: '#635FC7', borderColor: '#635FC7' }}><AddTaskIcon fontSize='small' /> Add New Task</Button>
-          <Tooltip title='Options' sx={{ml: 1}}>
-            <IconButton size='small' onClick={handleClick}>
-              <MoreVertOutlinedIcon htmlColor='#828FA3' fontSize='small' />
-            </IconButton>
-          </Tooltip>
-          <Menu anchorEl={anchorEl} open={open} onClose={() => setAnchorEl(null)}>
-            <MenuItem onClick={() => {setOpenEditModal(true); setAnchorEl(null)}} sx={{ color: '#828FA3' }}>Edit</MenuItem>
-            <MenuItem onClick={() => {setOpenDeleteModal(true); setAnchorEl(null)}} sx={{ color: '#828FA3' }}>Delete</MenuItem>
-          </Menu>
-        </Stack>
+        </Tooltip>
       </Stack>
-      <BoardModal isEditing boardName='Platform Launch' openModal={openEditModal} setOpenModal={setOpenEditModal}/>
-      <DeleteBoardModal itemName='Platform Launch' openModal={openDeleteModal} setOpenModal={setOpenDeleteModal} / >
-    </>
+      <Typography fontSize={[16, 24, 24]} fontWeight='bold'>Platform Launch</Typography>          
+      <Stack direction='row' alignItems='center'>
+        <IconButton sx={{display:['block','none']}} size='small' onClick={() => setOpenTaskModal(true)}>
+          <AddTaskIcon htmlColor='#635FC7' fontSize='medium' />
+        </IconButton>
+        <Button size='small' onClick={() => setOpenTaskModal(true)} variant='outlined' sx={{ textTransform: 'none', gap: 1, display: ['none', 'flex'], color: '#635FC7', borderColor: '#635FC7' }}><AddTaskIcon fontSize='small' /> Add New Task</Button>
+      </Stack>
+    </Stack>
   )
 }
 
