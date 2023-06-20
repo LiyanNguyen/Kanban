d44ccfbee0cc5c528508b541fb333f033939fd04
LiyanNguyen <liyannguyenn@gmail.com>
2023-06-20 21:30:05 +0800

    Added edit / delete board UI

--- src/components/DeleteBoardModal.tsx
+++ src/components/DeleteBoardModal.tsx
@@ -5,15 +5,13 @@ import CloseIcon from '@mui/icons-material/Close';
 import { modalStyle } from '../styles/modalStyle';
 
 type Props = {
-  type: 'Board' | 'Task'
   itemName: string
   openModal: boolean
   setOpenModal: Dispatch<SetStateAction<boolean>>
 }
 
 const DeleteBoardModal = (props: Props) => {
-  const { type, itemName, openModal, setOpenModal } = props
-
+  const { itemName, openModal, setOpenModal } = props
 
   return (
     <Modal disableAutoFocus open={openModal} onClose={() => setOpenModal(false)}>
@@ -21,13 +19,11 @@ const DeleteBoardModal = (props: Props) => {
         <IconButton sx={{ position: 'absolute', top: 8, right: 8 }} onClick={() => setOpenModal(false)}>
           <CloseIcon />
         </IconButton>
-        <Typography variant="h6" fontWeight='bold' color='error'>Delete This {type}?</Typography>
-        { type === 'Board' && 
-          <Typography color='gray'>Are you sure you want to delete the "{itemName}" board? This action will remove all columns and tasks and cannot be reversed.</Typography>
-        }
+        <Typography variant="h6" fontWeight='bold' color='error'>Delete This Board?</Typography>
+        <Typography color='gray'>Are you sure you want to delete the "{itemName}" board? This action will remove all columns and tasks and cannot be reversed.</Typography>
         <Stack direction='row' gap={2}>
           <Button variant='contained' color='error' fullWidth>Delete</Button>
-          <Button variant='contained' color='inherit' fullWidth>Cancel</Button>
+          <Button variant='contained' color='inherit' fullWidth onClick={() => setOpenModal(false)}>Cancel</Button>
         </Stack>
       </Stack>
     </Modal>
--- src/layouts/Topbar.tsx
+++ src/layouts/Topbar.tsx
@@ -1,8 +1,12 @@
-import { Box, Button, IconButton, Stack, Tooltip, Typography } from '@mui/material';
+import { Button, IconButton, Menu, MenuItem, Stack, Tooltip, Typography } from '@mui/material';
 import AddTaskIcon from '@mui/icons-material/AddTask';
 import { modalStore } from '../zustand/modalStore';
 import MenuOpenIcon from '@mui/icons-material/MenuOpenRounded';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState, MouseEvent} from 'react';
+import DeleteBoardModal from '../components/DeleteBoardModal';
+import BoardModal from '../components/BoardModal';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+
 
 type Props = {
   setOpen: Dispatch<SetStateAction<boolean>>
@@ -11,24 +15,46 @@ type Props = {
 const Topbar = (props: Props) => {
   const { setOpen } = props
   const [setOpenTaskModal] = modalStore((state) => [state.setOpenTaskModal])
+  const [openDeleteModal, setOpenDeleteModal] = useState<boolean>(false)
+  const [openEditModal, setOpenEditModal] = useState<boolean>(false)
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  }
 
   return (
-    <Stack direction='row' alignItems='center' p={3} bgcolor='white' borderBottom={'1px solid #E4EBFA'} justifyContent='space-between'>
-      <Stack direction='row' alignItems='center'>
-        <Tooltip title="Open Boards">
-          <IconButton size='small' onClick={() => setOpen(true)}>
-            <MenuOpenIcon htmlColor='#635FC7' fontSize='large' sx={{rotate: '180deg'}}/>
+    <>
+      <Stack direction='row' alignItems='center' px={[1,3,3]} py={1.5} bgcolor='white' borderBottom={'1px solid #E4EBFA'} justifyContent='space-between'>
+        <Stack direction='row' alignItems='center'>
+          <Tooltip title="Open Boards">
+            <IconButton size='small' onClick={() => setOpen(true)}>
+              <MenuOpenIcon htmlColor='#635FC7' fontSize='large' sx={{rotate: '180deg'}}/>
+            </IconButton>
+          </Tooltip>
+        </Stack>
+        <Typography fontSize={[16, 24, 24]} fontWeight='bold'>Platform Launch</Typography>          
+        <Stack direction='row' alignItems='center'>
+          <IconButton sx={{display:['block','none']}} size='small' onClick={() => setOpenTaskModal(true)}>
+            <AddTaskIcon htmlColor='#635FC7' fontSize='medium' />
           </IconButton>
-        </Tooltip>
+          <Button size='small' onClick={() => setOpenTaskModal(true)} variant='outlined' sx={{ textTransform: 'none', gap: 1, display: ['none', 'flex'], color: '#635FC7', borderColor: '#635FC7' }}><AddTaskIcon fontSize='small' /> Add New Task</Button>
+          <Tooltip title='Options' sx={{ml: 1}}>
+            <IconButton size='small' onClick={handleClick}>
+              <MoreVertOutlinedIcon htmlColor='#828FA3' fontSize='small' />
+            </IconButton>
+          </Tooltip>
+          <Menu anchorEl={anchorEl} open={open} onClose={() => setAnchorEl(null)}>
+            <MenuItem onClick={() => {setOpenEditModal(true); setAnchorEl(null)}} sx={{ color: '#828FA3' }}>Edit</MenuItem>
+            <MenuItem onClick={() => {setOpenDeleteModal(true); setAnchorEl(null)}} sx={{ color: '#828FA3' }}>Delete</MenuItem>
+          </Menu>
+        </Stack>
       </Stack>
-      <Typography variant='h5' fontWeight='bold'>Platform Launch</Typography>
-      <Box>
-        <IconButton sx={{display:['block','none']}} size='small' onClick={() => setOpenTaskModal(true)}>
-          <AddTaskIcon htmlColor='#635FC7' fontSize='medium' />
-        </IconButton>
-        <Button size='small' onClick={() => setOpenTaskModal(true)} variant='outlined' sx={{ textTransform: 'none', gap: 1, display: ['none', 'flex'], color: '#635FC7', borderColor: '#635FC7' }}><AddTaskIcon fontSize='small'/> Add New Task</Button>
-      </Box>
-    </Stack>
+      <BoardModal isEditing boardName='Platform Launch' openModal={openEditModal} setOpenModal={setOpenEditModal}/>
+      <DeleteBoardModal itemName='Platform Launch' openModal={openDeleteModal} setOpenModal={setOpenDeleteModal} / >
+    </>
   )
 }
 
